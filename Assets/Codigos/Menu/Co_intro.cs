d5067604dbd74560a9732ba35ad6abd8da4c0d4e
Rafael <rafael3do@hotmail.com>
2019-09-18 14:26:59 -0300

    Update
    Atualizado a intro do sistema

--- Assets/Codigos/Menu/Co_intro.cs
+++ Assets/Codigos/Menu/Co_intro.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Co_intro : MonoBehaviour
 {
     // Start is called before the first frame update
+    float tempo = 2.0f;
     void Start()
     {
         
     }
-
+    
     // Update is called once per frame
     void Update()
     {
+        tempo -= Time.deltaTime;
+            if (tempo < 0)
+            {
+                SceneManager.LoadScene("Ce_menu");
+            }
         
     }
 }
