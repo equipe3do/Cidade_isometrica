1a044948e52246c0a0e29f215e691229978ce9b9
Rafael <rafael3do@hotmail.com>
2019-09-27 10:13:22 -0300

    Test cursor and tilemap
    Make this scene play

---
+++ Assets/Codigos/Jogo/Co_controle_jogo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Co_controle_jogo : MonoBehaviour
+{
+    public Texture2D mouse;
+    public Vector2 hotspot = Vector2.zero;
+    public CursorMode cursormode = CursorMode.Auto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnGUI()
+    {
+        Cursor.SetCursor(mouse, hotspot, cursormode);
+    }
+}
